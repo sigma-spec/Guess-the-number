2f8275fc58d36c9211ea239cc6bcfcb3a7f12bf6
sigma-spec <68479240+sigma-spec@users.noreply.github.com>
2025-12-16 21:55:22 +0900

    guess_game.py

---
+++ guess_game.py
@@ -0,0 +1,22 @@
+import random
+
+secret_number = random.randint(1, 10)
+attempts = 3
+print("🎮 Guess the Number Game")
+print("I chose a number from 1 to 10.")
+print("You have 3 attempts.")
+
+for attempt in range(1, attempts + 1):
+    guess = int(input(f"Attempt {attempt}. Enter your number: "))
+
+    if guess == secret_number:
+        print("🔥 You guessed it! You win!")
+        break
+    else:
+        if guess > secret_number:
+            print("Too high!")
+        else:
+            print("Too low!")
+
+else:
+      print("❌ You lost. The number was:", secret_number)
